ff08757f9ca39ab971c299e772fec22a2da3c1c9
Ben <hostellerie.org@gmail.com>
2026-09-18 08:06:48 +0700

    Test Menu service discovery in Eclipse

--- quality/menu-navigation-test.php
+++ quality/menu-navigation-test.php
@@ -2,6 +2,48 @@
 
 define('VERSION', '2.2.2');
 
+
+define('PLG_RET_OK', 0);
+
+$GLOBALS['eclipse_test_service_calls'] = array();
+
+function PLG_invokeService($type, $action, $args, &$output, &$svc_msg)
+{
+    $GLOBALS['eclipse_test_service_calls'][] = $action;
+    $svc_msg = array();
+
+    if ($type !== 'menu') {
+        return -1;
+    }
+
+    if ($action === 'getMenuList') {
+        $output = array(
+            'provider' => 'menu',
+            'provider_family' => 'navigation',
+            'provider_contract_version' => 1,
+            'menus' => array(
+                array('id' => 1, 'name' => 'navigation', 'type' => 1),
+                array('id' => 2, 'name' => 'footer', 'type' => 2),
+            ),
+        );
+        return PLG_RET_OK;
+    }
+
+    if ($action === 'getMenuTree') {
+        $name = isset($args['name']) ? $args['name'] : '';
+        $output = array(
+            'provider' => 'menu',
+            'provider_family' => 'navigation',
+            'name' => $name,
+            'provider_contract_version' => 1,
+            'nodes' => MENU_getResolvedTree($name),
+        );
+        return PLG_RET_OK;
+    }
+
+    return -1;
+}
+
 function eclipse_menu_plugin_active()
 {
     return true;
@@ -91,6 +133,11 @@ function eclipse_test_assert($condition, $message)
     }
 }
 
+$menus = eclipse_menu_available_menus();
+eclipse_test_assert(count($menus) === 2, 'Menu service discovery count mismatch');
+eclipse_test_assert($menus[0]['name'] === 'navigation', 'Primary menu discovery missing');
+eclipse_test_assert($menus[1]['name'] === 'footer', 'Footer menu discovery missing');
+
 $html = eclipse_menu_navigation_resolved();
 
 eclipse_test_assert(strpos($html, 'class="eclipse-menu"') !== false, 'Eclipse wrapper missing');
@@ -129,5 +176,8 @@ eclipse_test_assert(count($filtered) === 2, 'unresolved top-level node was not f
 $header = file_get_contents(dirname(__DIR__) . '/eclipse/header.thtml');
 eclipse_test_assert(strpos($header, "require_once __DIR__") === false, 'header.thtml must not require theme files through __DIR__');
 eclipse_test_assert(strpos($header, "\$_CONF['path_layout']") !== false, 'header.thtml must use Geeklog path_layout for theme includes');
+
+eclipse_test_assert(in_array('getMenuList', $GLOBALS['eclipse_test_service_calls'], true), 'Eclipse did not use Menu list service');
+eclipse_test_assert(in_array('getMenuTree', $GLOBALS['eclipse_test_service_calls'], true), 'Eclipse did not use Menu tree service');
 
 echo "Eclipse resolved Menu navigation tests passed" . PHP_EOL;
